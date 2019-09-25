6e2750b129f25a66c8fbef648381589b6fa7c48b
gnx <gnx@example.com>
2019-09-25 21:30:43 +0800

    'aa'

--- youka/addOrder.py
+++ youka/addOrder.py
@@ -7,16 +7,30 @@ class UserBehavior(TaskSet):
 
     @task
     def test_register(self):
+
         url = "http://10.100.11.91/users/addorder"
-        print(self.locust.user_id)
-        self.client.post(url=url, data={"user_id": self.locust.user_id, "order_no": self.locust.order_no})
-        self.locust.user_id+=1
-        self.locust.order_no+=1
+        try:
+            user_id = self.locust.user_id_queue.get()
+            order_no = self.locust.order_no_queue.get()
+            print(user_id)
+        except queue.Empty:
+            print('account data run out, test ended.')
+            exit(0)
+        self.client.post(url=url, data={"user_id": user_id, "order_no": order_no})
+
+
 
 class WebsiteUser(HttpLocust):
     host = 'http://debugtalk.com'
     task_set = UserBehavior
-    user_id = 3569615076710976
-    order_no = 350908164200625888
+    user_id_queue = queue.Queue()
+    order_no_queue = queue.Queue()
+    user_id = 3369615076710976
+    order_no = 390908164200625888
+    for i in range(10000000):
+        user_id += 1
+        order_no += 1
+        user_id_queue.put_nowait(user_id)
+        order_no_queue.put_nowait(order_no)
     min_wait = 0
     max_wait = 0
