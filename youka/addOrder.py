18c4631c8463b670f7696bfaf1c40523869304cd
gnx <gnx@example.com>
2019-09-25 21:09:14 +0800

    'aaa'

--- youka/addOrder.py
+++ youka/addOrder.py
@@ -8,7 +8,7 @@ class UserBehavior(TaskSet):
     @task
     def test_register(self):
         url = "http://10.100.11.91/users/addorder"
-        print(user_id)
+        print(self.locust.user_id)
         self.client.post(url=url, data={"user_id": self.locust.user_id, "order_no": self.locust.order_no})
         self.locust.user_id+=1
         self.locust.order_no+=1
